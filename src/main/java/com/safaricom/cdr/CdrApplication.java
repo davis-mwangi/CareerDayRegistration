3548a714feefd5b4cfba5f38cfb5b300f07203d6
davis-mwangi <daudimwash@gmail.com>
2019-01-11 15:53:57 +0300

    Add Integrations tests

---
+++ src/main/java/com/safaricom/cdr/CdrApplication.java
@@ -0,0 +1,14 @@
+package com.safaricom.cdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdrApplication.class, args);
+	}
+
+}
+
